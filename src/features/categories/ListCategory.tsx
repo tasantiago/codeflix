e0ef767f0bdd0c6aa04b54393e46d81d89bd6c04
Thyago Alves Santiago <thyagoasantiago@gmail.com>
2023-01-19 10:20:36 -0400

    aula 08

--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -1,8 +1,9 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, IconButton, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useAppSelector } from "../../app/hooks";
 import { selectCategories } from "./categorySlice";
-import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
+import { DataGrid, GridColDef, GridRenderCellParams, GridRowsProp, renderActionsCell } from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 
 export const CategoryList = () => {
@@ -13,13 +14,49 @@ export const CategoryList = () => {
     id: category.id,
     name: category.name,
     description: category.description,
+    isActive: category.is_active, 
+    createdAt: new Date(category.created_at).toLocaleDateString('pt-BR'),
   }));
 
   const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 150 },
-    { field: 'name', headerName: 'Name', width:150},
-    { field: 'description', headerName: 'Description', width: 150 }
+    { field: 'name', headerName: 'Name', flex:1},
+    { 
+      field: 'isActive', 
+      headerName: 'Active', 
+      flex:1,
+      type:"boolean", 
+      renderCell: renderIsActiveCell
+    }, 
+    { 
+      field: 'createdAt', 
+      headerName: 'Created At', 
+      flex:1
+    },
+    { 
+      field: 'id', 
+      headerName: 'Actions', 
+      flex:1,
+      renderCell: renderActionsCell
+    },
   ];
+  function renderActionsCell(params: GridRenderCellParams){
+    return (
+      <IconButton
+        color="secondary"
+        onClick={() => console.log("clicked")}
+        aria-label="delete"
+      >
+        <DeleteIcon />
+      </IconButton>
+    )
+  }
+  function renderIsActiveCell(rowData:GridRenderCellParams){ 
+    return (
+      <Typography color={rowData.value ? "primary" : "secondary"}>
+        <span>{rowData.value ? "Active": "Inactive"}</span>
+      </Typography>
+      )
+  }
 
   return (
     <Box maxWidth="lg" sx={{mt:4,mb:4}}>
